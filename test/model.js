7ad8d0eb22ca23c57bc45e99549f41199965e105
Eldar Gabdullin <eldargab@gmail.com>
2012-08-25 20:52:42 +0400

    Implement model .toJSON(), .reset()

--- test/model.js
+++ test/model.js
@@ -1,12 +1,14 @@
 var Model = require('../lib/model')
 var sinon = require('sinon')
+var should = require('should')
 
 describe('Model', function () {
-    var m, l
+    var m, l, l2
 
     beforeEach(function () {
         m = new Model
         l = sinon.spy()
+        l2 = sinon.spy()
     })
 
     it('Supports getting and setting of attributes', function () {
@@ -28,5 +30,87 @@ describe('Model', function () {
         l.calledOnce.should.be.true
         m.set('foo', 'foo')
         l.calledTwice.should.be.true
+    })
+
+    it('Respects onset_<prop> setters', function () {
+        m.onset_foo = function (val, prop) {
+            val.should.equal('baz')
+            prop.should.equal('foo')
+            this.onset('foo', 'bar')
+        }
+        m.on('change:foo', l)
+        m.set('foo', 'baz')
+        m.get('foo').should.equal('bar')
+        l.calledOnce.should.be.true
+
+        m.set('baz', 'baz')
+        m.onset_baz = function () {}
+        m.on('change:baz', l2)
+        m.set('baz', 'hi')
+        m.get('baz').should.equal('baz')
+        l2.called.should.be.false
+    })
+
+    it('Respects onget_<prop> getters', function () {
+        m.onget_foo = function (prop) {
+            prop.should.equal('foo')
+            return 'FOO'
+        }
+        m.get('foo').should.equal('FOO')
+
+        m.set('bar', 'bar')
+        m.onget_baz = function () {
+            return this.onget('bar')
+        }
+        m.get('baz').should.equal('bar')
+    })
+
+    it('.toJSON()', function () {
+        m.set({
+            a: 'a',
+            b: 'b',
+            c: 'c'
+        })
+
+        m.on('json', function (json) {
+            delete json.a
+        })
+
+        m.toJSON().should.eql({
+            b: 'b',
+            c: 'c'
+        })
+
+        m.get('a').should.equal('a')
+    })
+
+    it('.reset()', function (done) {
+        m.set({
+            a: 'a',
+            b: 'b',
+            c: 'c'
+        })
+
+        m.on('change:a', l)
+
+        m.on('_reset_', function (attr) {
+            delete attr.x
+            attr.y = 'z'
+        })
+
+        m.on('reset', function () {
+            this.get('y').should.equal('z')
+            should.not.exist(this.get('a'))
+            should.not.exist(this.get('x'))
+            this.get('f').should.equal('f')
+            l.called.should.be.false
+            done()
+        })
+
+        m.reset({
+            x: 'x',
+            y: 'y',
+            f: 'f'
+        })
     })
 })
