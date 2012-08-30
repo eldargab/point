27c7d51031f83494bf73896fdb65611adb7a3df7
Eldar Gabdullin <eldargab@gmail.com>
2012-08-30 14:10:32 +0400

    fix: computable should clear cache on reset

--- test/mixin.computable.js
+++ test/mixin.computable.js
@@ -1,17 +1,19 @@
 var sinon = require('sinon')
 var Lib = require('../lib')
+var computable = Lib.mixin('computable')
 
 describe('Computable @mixin', function () {
-    it('Adds support for computable properties', function (done) {
-        var Model = Lib.Model.extend()
-        var computable = Lib.mixin('computable')
-        var onchange = sinon.spy()
+    var m, onchange
 
-        Model.use(computable, 'greeting', ['change:hello', 'change:world'], function () {
+    beforeEach(function () {
+        m =  new Lib.Model
+        onchange = sinon.spy()
+    })
+
+    it('Adds support for computable properties', function (done) {
+        m.use(computable, 'greeting', ['change:hello', 'change:world'], function () {
             return this.get('hello') + ' ' + this.get('world')
         })
-
-        var m = new Model
 
         m.on('change:greeting', onchange)
 
@@ -30,6 +32,41 @@ describe('Computable @mixin', function () {
 
         setTimeout(function () {
             onchange.calledOnce.should.be.true
+            done()
+        }, 10)
+    })
+
+    it('Computable value is cached', function () {
+        var calls = 0
+        m.use(computable, 'foo', ['bar'], function () {
+            calls++
+            return 'foo'
+        })
+        m.get('foo')
+        calls.should.equal(1)
+        m.get('foo')
+        calls.should.equal(1)
+
+        m.emit('bar')
+        m.get('foo')
+        calls.should.equal(2)
+    })
+
+    it('Cache is cleared on reset', function (done) {
+        m.use(computable, 'foo', [], function () {
+            return !!this.get('baz')
+        })
+
+        m.on('change:foo', onchange)
+
+        m.get('foo').should.equal(false)
+
+        m.reset({baz: true})
+
+        m.get('foo').should.equal(true)
+
+        setTimeout(function () {
+            onchange.called.should.be.false
             done()
         }, 10)
     })
